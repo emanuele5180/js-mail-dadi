d49b366d7d7ecd110ddf5bff68173b52da75027f
emanuele5180 <macina5180>
2020-06-24 00:48:31 +0200

    mail1

--- mail/main.js
+++ mail/main.js
@@ -1,13 +1,22 @@
 //Per quello delle mail far inserire all'utente una mail e controllare se è nell'array, che contiene quelle valide. Dare all'utente un messaggio opportuno.
 var elencoMailValide = ["topolino@uno.com", "pippo@uno.com", "pluto@uno.com"];
 var mail = prompt( "inserisci la tua mail");
+var mailTrovata = false;
 
 for (var i = 0; i < elencoMailValide.length; i++) {
-  console.log(elencoMailValide[i]);
+  //console.log(elencoMailValide[i]);
+
+  if (mail == elencoMailValide[i]) {
+    mailTrovata = true;
+    //console.log("mail corretta");
+
+  }
+
 }
 
-if (mail == elencoMailValide[i]) {
+if (mailTrovata = elencoMailValide) {
   console.log("mail corretta");
-}else {
-  console.log("rifai")
+
+} else {
+  console.log("rifai!");
 }

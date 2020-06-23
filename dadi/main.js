bdd3c77b4180caedc219524e162f4d33d36527cf
emanuele5180 <macina5180>
2020-06-23 15:32:14 +0200

    esercizio1

---
+++ dadi/main.js
@@ -0,0 +1,17 @@
+// si lanciano due dadi, uno per il computer e uno per il giocatore e si determina chi ha vinto.
+
+document.getElementById("launch").addEventListener("click", function(){
+  var dado1 = Math.floor( Math.random ()*6);
+  var dado2 = Math.floor( Math.random ()*6);
+  //console.log(dado1);
+  //console.log(dado2);
+  if (dado1 > dado2) {
+    console.log("hai vinto giocatore 1");
+
+  }else if (dado2 > dado1){
+    console.log("hai vinto Skynet");
+
+  }else {
+    console.log("pari! rifai");
+  }
+});
